dfca0f5bbebfe11d9c5d8bf6584bd1bf80d62274
Anhgelus Morhtuuzh <william@herges.fr>
2025-10-02 17:36:27 +0200

    feat(backend): og and twitter seo

--- backend/data.go
+++ backend/data.go
@@ -1,11 +1,41 @@
 package backend
 
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
 type data struct {
-	title   string
-	Article bool
+	title       string
+	Article     bool
+	Domain      string
+	URL         string
+	Image       string
+	Description string
 }
 
-func (d data) Title() string {
+func (d *data) handleGeneric(w http.ResponseWriter, name string) {
+	t, err := template.New("").Funcs(template.FuncMap{
+		"static": func(path string) string {
+			return fmt.Sprintf("/static/%s", path)
+		},
+		"assets": func(path string) string {
+			return fmt.Sprintf("/assets/%s", path)
+		},
+	}).ParseFS(templates, fmt.Sprintf("templates/%s.html", name), "templates/base.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	err = t.ExecuteTemplate(w, "base.html", d)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func (d *data) Title() string {
 	title := "anhgelus"
 	if d.Article {
 		title += " - log entry"
--- backend/home.go
+++ backend/home.go
@@ -1,18 +1,21 @@
 package backend
 
 import (
-	"html/template"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func HandleHome(r *chi.Mux) {
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		t := template.Must(template.ParseFS(templates, "templates/home.html", "templates/base.html"))
-		err := t.ExecuteTemplate(w, "base.html", &data{})
-		if err != nil {
-			panic(err)
+	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
+		d := &data{
+			title:       "",
+			Article:     false,
+			Domain:      "anhgelus.world",
+			URL:         "/",
+			Image:       "",
+			Description: "",
 		}
+		d.handleGeneric(w, "home")
 	})
 }
