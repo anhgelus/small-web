270f592fc65cd5553ccf7c0dc81431c3efdf67ab
William Hergès <william@herges.fr>
2025-10-03 17:21:29 +0200

    feat(frontend): bundle htmx and set every external link to target blank

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import 'htmx.org';
+
+document.querySelectorAll("a").forEach(e => {
+    if (e.href.startsWith(window.location.origin)) return;
+    e.target = "_blank";
+});
